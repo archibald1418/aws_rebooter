778856f728fd6e974026b96d9171c6d937513c12
archibald1418 <liam141810@gmail.com>
2024-02-11 16:53:09 +0000

    display python-shell's executable

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 import {PythonShell} from 'python-shell';
 import 'dotenv/config'; // load .env variables
+const {exec} = await import('child_process');
 
 
 // PythonShell.runString('x=1+1;print(x)', null).then(messages=>{
 //   console.log('finished');
 // });
+
+exec('which python', (err, stdout, stderr) => {
+  if (err) {
+    console.error(err);
+  } else {
+  console.log(stdout);
+  }
+});
 
 const options = {
   mode : "text",
