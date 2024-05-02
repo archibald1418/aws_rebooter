967961afc292b42571f8cec2aa3dbb8cab910383
archibald1418 <liam141810@gmail.com>
2024-05-02 01:05:44 +0100

    from tuple ctor

--- api/src/modules/entity.py
+++ api/src/modules/entity.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from typing import Self
 
 class UserModel(BaseModel):
     id_hash: int
@@ -11,6 +12,10 @@ class UserEntity:
     
     def as_tuple(self) -> tuple[int, bool]:
         return (self.id_hash, self.admin)
+    
+    @classmethod
+    def from_tuple(cls, args: tuple[int, bool]) -> Self:
+        return cls(*args)
 
     def __repr__(self):
         return f'<User({self.id_hash}, {self.admin})>'
