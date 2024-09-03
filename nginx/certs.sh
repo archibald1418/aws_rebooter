3b9a0db6b3b1773680911b39d0f7aaed51e72715
archibald1418_aws <liam141810@gmail.com>
2024-09-03 02:42:43 +0000

    api depends on nginx but api should wait for certs; a healthcheck script?

--- nginx/certs.sh
+++ nginx/certs.sh
@@ -3,24 +3,27 @@
 
 FLAGS="--debug -v --nginx -d $HOST --register-unsafely-without-email"
 
-if [ $BUILD = 'dev' ]; then
-	FLAGS+= " --test-cert"
+if [ "$BUILD" == "dev" ]; then
+	FLAGS="$FLAGS --test-cert";
+	# http only..
 fi
 
 
 if [ ! -d /etc/letsencrypt/live/$HOST ]; then
     envsubst '${HOST}' < template.conf > /etc/nginx/conf.d/default.conf;
-    yes 'y' | certbot $FLAGS
-	# TODO: test script with --staging
+    yes 'y' | certbot $FLAGS;
+	# --test-cert gets invalid cert, so can only test with http
+	# letsencrypt has two limits: duplicate cert limit and cert limit
+    # 10 certs for 3 hours..
 else
-    echo 'Certs already installed'
+    echo 'Certs already installed';
 fi
 
 service nginx stop
 
 echo 'Starting nginx...'
 if ! nginx -g 'daemon off;'; then
-    echo 'Nginx failed'
-    echo 'keep-alive container'
-    tail -f /dev/null
+    echo 'Nginx failed';
+    echo 'keep-alive container';
+    tail -f /dev/null;
 fi
